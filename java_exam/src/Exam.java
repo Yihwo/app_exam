26bb8f75bb6ee75b87bc6fa3b5e603fbc333e36f
501-09-PC\501-09 <hosup1124@gmail.com>
2018-01-16 11:16:45 +0900

    값의 연산

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -1,6 +1,6 @@
 public class Exam {
     public static void main(String[] args){
-        int num1;
+        /*int num1;
         num1=10;
         int num2 = 20;
         int num3 = num1+num2;
@@ -10,5 +10,20 @@ public class Exam {
         System.out.println(text);
         double a = 0.12;
         System.out.println(a);
+        */
+        int n1 =7, n2 = 3;
+        int result = n1 + n2;
+        System.out.println("덧셈 결과 : " + result);
+        result = n1 - n2;
+        System.out.println("뺄셈 결과 : " + result);
+        System.out.println("곱셈 결과 : " + n1*n2);
+        System.out.println("나눗셈 결과 : " + n1/n2);
+        System.out.println("나머지 결과 : " + n1%n2);
+
+        int num01, num02, num03;
+        num01 = 30; num02 = 60; num03 = 90;
+        result = num01+num02+num03;
+        System.out.println("합계 : " + result);
+        System.out.println("평균 : " + result/3);
     }
 }
