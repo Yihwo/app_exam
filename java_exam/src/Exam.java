3dcc8943d4ecd72ab7502d138928410c25a54f98
501-09-PC\501-09 <hosup1124@gmail.com>
2018-01-17 13:00:54 +0900

    if문 예제

--- java_exam/src/Exam.java
+++ java_exam/src/Exam.java
@@ -21,7 +21,7 @@ public class Exam {
         System.out.println("나머지 결과 : " + n1%n2);
 
         int num01, num02, num03;
-        num01 = 30; num02 = 60; num03 = 90;
+        num01 = 33; num02 = 70; num03 = 90;
         result = num01+num02+num03;
         System.out.println("합계 : " + result);
         System.out.println("평균 : " + result/3);
@@ -39,6 +39,36 @@ public class Exam {
         System.out.println("결과 : "+ resultB);
         resultB = num01!=num02;
         System.out.println("결과 : "+ resultB);
+        if(num01>num02){
+            System.out.println("큰 값은 "+num01+"입니다.");
+        }
+        else{
+            System.out.println("큰 값은 "+num02+"입니다.");
+        }
+        if(num01>0){
+            System.out.println(num01+"은 0보다 크다.");
+        }
+        if((num01%2)==0){
+            System.out.println(num01+"은 짝수");
+        }
+        else{
+            System.out.println(num01+"은 홀수");
+        }
+        if(num02>=90) {
+            System.out.println("성적은 수 입니다.");
+        }
+        else if(num02>=80){
+            System.out.println("성적은 우 입니다.");
+        }
+        else if(num02>=70){
+            System.out.println("성적은 미 입니다.");
+        }
+        else if(num02>=60){
+            System.out.println("성적은 양 입니다.");
+        }
+        else{
+            System.out.println("성적은 가 입니다.");
+        }
 
     }
 }
