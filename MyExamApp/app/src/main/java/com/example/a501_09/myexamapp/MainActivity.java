485ef9ca8beb997ed038c21af707a850eb11b46b
501-09-PC\501-09 <hosup1124@gmail.com>
2018-02-26 16:18:35 +0900

    버튼과 리스너 연결하기

--- MyExamApp/app/src/main/java/com/example/a501_09/myexamapp/MainActivity.java
+++ MyExamApp/app/src/main/java/com/example/a501_09/myexamapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a501_09.myexamapp;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,25 +9,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//앱을 켜자마자 실행되는 함수
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //컴포넌트의 객체 생성
+        Button btn_test = (Button)findViewById(R.id.button1);//레이아웃의 버튼이 기능할 수 있도록 코드의 버튼 생성
+        Button btn_test02 = (Button)findViewById(R.id.button2);
+        ImageView image01 = (ImageView)findViewById(R.id.image1);
+        //리스너의 객체 생성
+        MyListener01 myListener = new MyListener01();
+        MyListener02 myListener02 = new MyListener02();
+        //컴포넌트 객체에 리스너 등록
+        btn_test.setOnClickListener(myListener);
+        btn_test02.setOnClickListener(myListener02);
+        image01.setOnClickListener(myListener);
     }
+
+    //버튼이 기능할 리스너 만듦
+    class MyListener01 implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            //텍스트 출력
+            Toast.makeText(MainActivity.this,"버튼을 클릭하였습니다.",Toast.LENGTH_SHORT).show();
+        }
+    }
+    class MyListener02 implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            //텍스트 출력
+            Toast.makeText(MainActivity.this,"버튼2를 클릭하였습니다.",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
