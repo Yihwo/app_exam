50d0e97dd229b077439a8b87b59879ed9b55604a
501-09-PC\501-09 <hosup1124@gmail.com>
2018-02-27 17:39:10 +0900

    블팬 back버튼 추가

--- ActivityExam/app/src/main/java/com/example/a501_09/activityexam/VideoActivity.java
+++ ActivityExam/app/src/main/java/com/example/a501_09/activityexam/VideoActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.RatingBar;
@@ -20,11 +21,16 @@ public class VideoActivity extends AppCompatActivity {
     VideoView bp_Video;
     RatingBar ratingBar;
     ImageView bp_Image;
+    ImageButton back_btn;
     @Override
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        back_btn = (ImageButton)findViewById(R.id.imageBtn_Back);
+        BackBtnListener backBtnListener = new BackBtnListener();
+        back_btn.setOnClickListener(backBtnListener);
 
         bp_Image = (ImageView)findViewById(R.id.image_Bp);
         ImageListener imageListener = new ImageListener();
@@ -55,6 +61,13 @@ public class VideoActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             Toast.makeText(VideoActivity.this,"Black Panther",Toast.LENGTH_SHORT).show();
+        }
+    }
+    class BackBtnListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            finish();
         }
     }
 
