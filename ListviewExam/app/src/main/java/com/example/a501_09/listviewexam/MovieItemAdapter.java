056409dbcc9811834d5c3d13afd4bcb190ee3f37
501-09-PC\501-09 <hosup1124@gmail.com>
2018-03-13 17:37:38 +0900

    영화 앱에 리스트 항목 편집 적용

---
+++ ListviewExam/app/src/main/java/com/example/a501_09/listviewexam/MovieItemAdapter.java
@@ -0,0 +1,31 @@
+package com.example.a501_09.listviewexam;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 501-09 on 2018-03-13.
+ */
+
+public class MovieItemAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return view;
+    }
+}
