4a3e3bb8efb83b0c13d9eb9310344e99716e6006
501-09-PC\501-09 <hosup1124@gmail.com>
2018-03-19 14:25:30 +0900

    xml 추가

--- XMLExample/app/src/main/java/com/example/a501_09/xmlexample/MainActivity.java
+++ XMLExample/app/src/main/java/com/example/a501_09/xmlexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView txt_main = (TextView)findViewById(R.id.textview_main);
+        //xml 데이터 //보통 인터넷 혹은 파일로 데이터를 받아옴
         String xml_str = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
                 "<root>\n" +
                 "\t<item Maker=\"청주\">사과</item>\n" +
@@ -40,16 +42,25 @@ public class MainActivity extends AppCompatActivity {
             //입력 받은 데이터를 byte화
             InputStream input = new ByteArrayInputStream(xml_str.getBytes("utf-8"));
 
+            //데이터가 입력되어있는 태그 중 가장 바깥 태그를 받아옴
             Document document = builder.parse(input);
-            //가장 큰 태그를 가져옴
             Element root = document.getDocumentElement();
-            //항목들을 nodelist에 저장
+            //root 태그 안의 item 항목들을 nodelist에 저장
             NodeList items = root.getElementsByTagName("item");
             String buf="";
+            String buf_1 = "";
             for (int i = 0;i<items.getLength();i++){
-                //nodelist의 항목들을 각각 떼어내 node에 저장
+                //item 태그의 값들을 각각 node에 저장
                 Node item = items.item(i);
                 String value = item.getFirstChild().getNodeValue();
+
+                //노드의 속성값 불러오기
+                NamedNodeMap attr = item.getAttributes();
+                for (int j = 0; j<attr.getLength();j++){
+                    Node attr_node = attr.item(j);
+                    buf += attr_node.getNodeValue()+" ";
+                }
+
                 buf += value+" ";
                 txt_main.setText(buf);
             }
