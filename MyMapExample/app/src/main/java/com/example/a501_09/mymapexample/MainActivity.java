28242ae2415d433da30d41c4a9f796e57f13d5ab
501-09-PC\501-09 <hosup1124@gmail.com>
2018-03-27 15:51:06 +0900

    맵 사용 환경 설정

---
+++ MyMapExample/app/src/main/java/com/example/a501_09/mymapexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a501_09.mymapexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //AIzaSyDZx-5PxLLpR6aeXGx6GHgCtqQDiyeGJs4
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
