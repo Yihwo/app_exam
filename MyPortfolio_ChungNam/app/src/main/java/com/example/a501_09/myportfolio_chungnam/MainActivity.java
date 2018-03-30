f6171ccb069da69a16d947659608a3c326267eb1
501-09-PC\501-09 <hosup1124@gmail.com>
2018-03-30 11:02:29 +0900

    웹뷰 /화면 고정

---
+++ MyPortfolio_ChungNam/app/src/main/java/com/example/a501_09/myportfolio_chungnam/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.a501_09.myportfolio_chungnam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setMainWebView();
+    }
+    private void setMainWebView(){
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+
+        //자바 스크립트가 정상적으로 동작하도록 해줌
+        webSettings.setJavaScriptEnabled(true);
+        //앱에서 웹뷰를 출력하도록 도와줌
+        webView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view,String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl("http://www.naver.com");
+    }
+}
