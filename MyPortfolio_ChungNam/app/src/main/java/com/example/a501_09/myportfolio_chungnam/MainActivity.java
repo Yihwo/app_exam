cdc9ebf3491bcdfb5b96daad5e0b2c7c12addaad
501-09-PC\501-09 <hosup1124@gmail.com>
2018-03-30 15:07:49 +0900

    웹뷰에서 링크 클릭을 통한 activity이동 / 권한 허용 코드

--- MyPortfolio_ChungNam/app/src/main/java/com/example/a501_09/myportfolio_chungnam/MainActivity.java
+++ MyPortfolio_ChungNam/app/src/main/java/com/example/a501_09/myportfolio_chungnam/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a501_09.myportfolio_chungnam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
@@ -23,10 +24,16 @@ public class MainActivity extends AppCompatActivity {
         //앱에서 웹뷰를 출력하도록 도와줌
         webView.setWebViewClient(new WebViewClient(){
             public boolean shouldOverrideUrlLoading(WebView view,String url){
-                view.loadUrl(url);
+                //연결된 홈페이지 내부에서 특정한 url을 선택 했을때 activity를 이동함
+                if(url.startsWith("app://")){
+                    Intent intent = new Intent(MainActivity.this,TestActivity.class);
+                    startActivity(intent);
+                }else{
+                    view.loadUrl(url);
+                }
                 return true;
             }
         });
-        webView.loadUrl("http://www.naver.com");
+        webView.loadUrl("http://118.46.60.101/index.php");
     }
 }
--- MyPortfolio_ChungNam/app/src/main/java/com/example/a501_09/myportfolio_chungnam/SplashActivity.java
+++ MyPortfolio_ChungNam/app/src/main/java/com/example/a501_09/myportfolio_chungnam/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.example.a501_09.myportfolio_chungnam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by 501-09 on 2018-03-30.
@@ -12,5 +14,16 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //앱을 동작하기 위한 준비를 위한 시간
+        try{
+            Thread.sleep(5000);
+            Toast.makeText(this, "5초 ", Toast.LENGTH_SHORT).show();
+        }catch(Exception e){;}
+
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 }
