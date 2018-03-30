5de54de248548aec40740ab9c15c6e25e172339f
501-09-PC\501-09 <hosup1124@gmail.com>
2018-03-30 12:20:33 +0900

    포트폴리오 앱 스플래쉬 적용

---
+++ MyPortfolio_ChungNam/app/src/main/java/com/example/a501_09/myportfolio_chungnam/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.a501_09.myportfolio_chungnam;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 501-09 on 2018-03-30.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
